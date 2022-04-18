eb1c2afa4e6c072104c204dfd71f4fa5fa7dabab
SEOYEON HAN <tjdus0824@naver.com>
2022-04-19 02:03:25 +0900

    4월 19일 저장

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import Modal from 'components/modal/Modal';
 import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSmile } from '@fortawesome/free-solid-svg-icons';
 // import { Link } from "react-router-dom";
 // import { useDispatch } from "react-redux";
 
@@ -12,7 +14,7 @@ const LoginBox = styled.div`
   display: inline-block;
   width: 350px;
   height: 450px;
-  border: 2px solid #e4e4e4;
+  /* border: 2px solid #adadad; */
   border-radius: 8px;
   text-align: center;
   flex-direction: column;
@@ -21,55 +23,72 @@ const LoginBox = styled.div`
 
 const Email = styled.input`
   width: 250px;
-  padding: 13px;
+  padding: 14px;
   margin: 25px 10px 5px 10px;
-  border: 1px solid gray;
+  border: 1px solid #adadad;
   border-radius: 5px;
   background-color: #f6f6f6;
 `;
 
 const Password = styled.input`
   width: 250px;
-  padding: 13px;
+  padding: 14px;
   margin: 10px;
-  border: 1px solid gray;
+  border: 1px solid #adadad;
   border-radius: 5px;
   background-color: #f6f6f6;
 `;
 
 const LoginBtn = styled.button`
   width: 90%;
-  margin-top: 20px;
-  padding: 10px 0px;
+  margin: 20px 5px 0px 0px;
+  padding: 11px 0px;
   cursor: pointer;
   color: white;
   font-size: 17px;
   font-weight: 550;
   border-radius: 5px;
-  border: 1px solid lightgray;
+  border: 1px solid #adadad;
   background-color: #ff8a3d82;
   ${({ login }) => {
     return login ? `background-color: #ff8a3d82;` : `background-color: #red;`;
   }}
 `;
-// LoginBtn.defaultProps = {
-//   btnDisabled : {
-//     main : 'palevioletred'
-//   }
-// }
-// const btnDisabled = {
-//   main: 'palevioletred'
-// }
+
+const Save = styled.p`
+  margin: 5px 190px 0px 0px;
+  font-size: 13px;
+`;
+const Icon = styled.svg`
+  fill: none;
+  stroke: white;
+  stroke-width: 2px;
+`;
+
+const StyledCheckBox = styled.div`
+  display: inline-block;
+  width: 2rem;
+  height: 2rem;
+  border: ${props => (props.checked ? 'none' : 'solid 0.1rem #dddddd')};
+  background: ${props => (props.checked ? 'black' : 'white')};
+  border-radius: 0.4rem;
+  transition: all 150ms;
+  ${Icon} {
+    visibility: ${props => (props.checked ? 'visible' : 'hidden')};
+  }
+`;
 
 const Usersign = styled.div`
-  font-size: 15px;
-  margin-top: 180px;
+  border-top: 1px solid #adadad;
+  font-size: 14px;
+  margin-top: 140px;
+  padding-top: 30px;
   text-align: center;
 `;
 
 const Usersignup = styled.span`
   color: #ff8a3d;
-  font-size: 15px;
+  font-size: 14px;
   margin-left: 10px;
   cursor: pointer;
 `;
@@ -112,12 +131,12 @@ function Login() {
   };
 
   return (
-    <Modal width="373px">
+    <Modal width="365px">
       <LoginBox width="200px">
         <img
           src={`${CLIENT_PORT}/images/logo/logo1.png`}
           alt="logo"
-          width="200px"
+          width="225px"
         />
         <Email
           value={inputId}
@@ -138,6 +157,9 @@ function Login() {
         >
           로그인
         </LoginBtn>
+        <StyledCheckBox></StyledCheckBox>
+        <Save>로그인 상태 유지</Save>
+        {/* <FontAwesomeIcon icon={faSmile} /> */}
         <Usersign>
           아직 회원이 아니신가요?
           <Usersignup onClick={handleSignIn}>회원가입</Usersignup>
