3af7e1a08c9740d5ba493d966c4ed5a506fa5d57
shon97 <wjdgns200297@gmail.com>
2022-04-18 15:36:07 +0900

    ADD: logo

--- src/nav/Header.js
+++ src/nav/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { CLIENT_PORT } from "config";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <img
+        src={`${CLIENT_PORT}/images/logo/logo.png`}
+        alt="logo"
+        width="24px"
+      />
+    </div>
+  );
 }
 
 export default Header;
