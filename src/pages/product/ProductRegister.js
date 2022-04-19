90129511199290b57b0291e9b7295c04d65f9bb3
Chae Won Suh <chaewon@chaewon-ui-Mac.local>
2022-04-19 10:38:45 +0900

    판매 등록 페이지 이미지 프리뷰 기능 구현

--- src/pages/product/ProductRegister.js
+++ src/pages/product/ProductRegister.js
@@ -3,32 +3,62 @@ import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import { BsFillCameraFill } from 'react-icons/bs';
+import { TiDelete } from 'react-icons/ti';
 
 const Register = () => {
-  const [selectedImage, setSelectedImage] = useState([]);
-  const [imageURLs, setImageURLs] = useState([]);
+  const [selectedImage, setSelectedImage] = useState([]); //업로드한 이미지들을 저장
+  const [imageURLs, setImageURLs] = useState([]); //이미지 src를 저장
   const hiddenFileInput = useRef(null);
 
   const handleClick = event => {
     hiddenFileInput.current.click();
-  };
+  }; //버튼을 눌렀을때 이벤트가 실행되게 함
 
   useEffect(() => {
     if (selectedImage.length < 1) {
       return;
     }
-    const newImageURLs = [];
-    selectedImage.forEach(image =>
-      newImageURLs.push(URL.createObjectURL(image))
-    );
-    setImageURLs(newImageURLs);
-  }, [selectedImage]);
+    if (selectedImage.length > 10) {
+      alert('사진을 10개를 초과할 수 없어요');
+      setSelectedImage([]);
+      setImageURLs([]);
+    } else {
+      const newImageURLs = [];
+      selectedImage.forEach(image =>
+        newImageURLs.push(URL.createObjectURL(image))
+      );
+      setImageURLs(newImageURLs);
+    }
+  }, [selectedImage, imageURLs]);
+
+  // useEffect(() => {
+  //   if (selectedImage.length < 1) {
+  //     return;
+  //   }
+  //   if (selectedImage.length > 10) {
+  //     alert('사진을 10개를 초과할 수 없어요');
+  //     setSelectedImage([]);
+  //     setImageURLs([]);
+  //   } else {
+  //     selectedImage.forEach(image =>
+  //       setImageURLs([...imageURLs, URL.createObjectURL(image)])
+  //     );
+  //   }
+  // }, [selectedImage]);
 
   const onImageChange = event => {
-    // const fileUploaded = event.target.files[0];
-    // hiddenFileInput.handleFile(fileUploaded);
     setSelectedImage([...event.target.files]);
   }; //파일 업로드
+
+  const deleteImage = src => {
+    let imageIndex = imageURLs.indexOf(src);
+    selectedImage.splice(imageIndex, 1);
+    imageURLs.splice(imageIndex, 1);
+    if (imageURLs.length === 1) {
+      imageURLs.pop(0);
+      selectedImage.pop(0);
+    }
+  };
 
   return (
     <div>
@@ -49,6 +79,11 @@ const Register = () => {
         </PhotoButton>
         {imageURLs.map(imageSrc => (
           <div className="imageContainer">
+            <TiDelete
+              className={'photoDiscard'}
+              value={imageSrc}
+              onClick={() => deleteImage(imageSrc)}
+            />
             <img src={imageSrc} className={'eachImage'} />
           </div>
         ))}
@@ -59,16 +94,33 @@ const Register = () => {
 
 const PhotoLine = styled.div`
   display: flex;
-  height: 10vw;
+  justify-content: flex-start;
+  overflow-x: auto;
+  width: auto;
+  height: 150px;
   border: 1px solid black;
-  margin-bottom: 2%;
+  padding: 2vh;
+  margin: 0 1vw 1vw 1vw;
 
   .imageContainer {
-    width: 10vw;
-    margin-left: 1%;
+    display: flex;
+    align-items: center;
+    position: relative;
+    width: auto;
+    height: 100%;
+    margin-left: 2vh;
     aspect-ratio: 1/1;
-    border-radius: 10px;
+
+    .photoDiscard {
+      position: absolute;
+      right: -2.5vh;
+      top: -2.5vh;
+      z-index: 10;
+      font-size: 5vh;
+      cursor: pointer;
+    }
   }
+
   .eachImage {
     width: 100%;
     height: 100%;
@@ -85,14 +137,14 @@ const PhotoLine = styled.div`
 
 const PhotoButton = styled.button`
   display: flex;
+  aspect-ratio: 1/1;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 10%;
-  height: auto;
+  width: auto;
+  height: 100%;
   background-color: white;
   border: 1px solid lightgray;
-  margin-left: 1%;
   border-radius: 10px;
 
   :hover {
