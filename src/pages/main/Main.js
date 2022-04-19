5f5492f941c09cb6acc31d524f20fad12bb574b6
shon97 <wjdgns200297@gmail.com>
2022-04-19 12:37:32 +0900

    FIX: main styles

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -13,7 +13,15 @@ function Main() {
   return (
     <MainWrapper>
       <TopArticle>
-        <img src={`${CLIENT_PORT}/images/thump/banner-a.png`} alt="banner" />
+        <section>
+          <h1>당신 근처의</h1>
+          <h1>당근 마켓</h1>
+          <p>중고 거래부터 동네 정보까지, 이웃과 함께해요.</p>
+          <span>가깝고 따뜻한 당신의 근처를 만들어요.</span>
+        </section>
+        <section>
+          <img src={`${CLIENT_PORT}/images/thump/banner-a.png`} alt="banner" />
+        </section>
       </TopArticle>
       <BottomArticle>
         <section>
