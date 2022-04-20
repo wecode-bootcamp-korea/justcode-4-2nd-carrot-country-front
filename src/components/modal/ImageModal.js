ea6715b62453a5ea38ec273e2545d0f847f4bb26
shon97 <wjdgns200297@gmail.com>
2022-04-20 17:31:51 +0900

    ADD: 이미지 모달/ 이미지 클릭 시 슬라이더 포함

--- src/components/modal/ImageModal.js
+++ src/components/modal/ImageModal.js
@@ -1,9 +1,58 @@
+import { CLIENT_PORT } from 'config';
 import React from 'react';
+
+import { MdClose } from 'react-icons/md';
 
 import styled from 'styled-components';
 
-function ImageModal() {
-  return <div>ImageModal</div>;
+function ImageModal(props) {
+  const { urls, visible, setVisible } = props;
+  return (
+    <MainWrapper visible={visible}>
+      <Container>
+        <MdClose size={34} onClick={() => setVisible(false)} />
+        {urls.map((url, idx) => {
+          return (
+            <img src={CLIENT_PORT + url} alt={url} width={670} key={idx} />
+          );
+        })}
+      </Container>
+    </MainWrapper>
+  );
 }
+
+const MainWrapper = styled.div`
+  display: ${props => (props.visible ? 'block' : 'none')};
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: #000;
+`;
+
+const Container = styled.div`
+  @media (max-width: 890px) {
+  }
+  @media (min-width: 891px) {
+    width: 670px;
+    margin: auto;
+  }
+  background-color: #ffff;
+  svg {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    color: #999;
+  }
+  img {
+    width: 100%;
+  }
+  img:not(:last-child) {
+    margin-bottom: 20px;
+  }
+`;
 
 export default ImageModal;
--- src/components/slider/ImageSlider.js
+++ src/components/slider/ImageSlider.js
@@ -1,75 +1,174 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CLIENT_PORT } from 'config';
+
+import ImageModal from 'components/modal/ImageModal';
 
 import {
   MdOutlineArrowBackIos,
   MdOutlineArrowForwardIos,
 } from 'react-icons/md';
+import { BsFillCircleFill } from 'react-icons/bs';
 
 import styled from 'styled-components';
 
 function ImageSlider(props) {
   const { urls, width, height } = props;
   const [index, setIndex] = useState(0);
+  const [useVisible, setUseVisible] = useState(false);
+  const [useWidth, setUseWidth] = useState(
+    window.innerWidth > 890 ? width : window.innerWidth
+  );
 
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  const handleResize = () => {
+    setUseWidth(window.innerWidth);
+  };
   const handlePrev = () => {
     if (index > 0) {
       setIndex(index - 1);
     }
   };
   const handleNext = () => {
-    // 이미지 길이보다 작다는 조건문 추가
-    setIndex(index + 1);
+    if (urls.length !== index + 1) {
+      setIndex(index + 1);
+    }
   };
-
+  console.log(useWidth);
   return (
     <MainWrapper width={width}>
-      <MdOutlineArrowBackIos size={28} onClick={handlePrev} />
-      <ImageWrapper index={index} width={width} height={height}>
+      <MdOutlineArrowBackIos
+        className="prevBtn"
+        size={30}
+        onClick={handlePrev}
+      />
+      <ImageWrapper
+        index={index}
+        width={width}
+        changeWidth={useWidth}
+        height={height}
+      >
         {urls.map((url, idx) => {
           return (
-            <div key={idx}>
+            <div key={idx} onClick={() => setUseVisible(true)}>
               <img src={CLIENT_PORT + url} alt={url} />
             </div>
           );
         })}
+        <DotWrapper width={width} changeWidth={useWidth}>
+          {urls.map((__, idx) => {
+            const isCurrent = index === idx;
+            return (
+              <DotItem key={idx} isCurrent={isCurrent}>
+                <BsFillCircleFill size={8} />
+              </DotItem>
+            );
+          })}
+        </DotWrapper>
       </ImageWrapper>
-      <MdOutlineArrowForwardIos size={28} onClick={handleNext} />
+      <MdOutlineArrowForwardIos
+        size={30}
+        onClick={handleNext}
+        className="nextBtn"
+      />
+      <ImageModal urls={urls} visible={useVisible} setVisible={setUseVisible} />
     </MainWrapper>
   );
 }
 
 const MainWrapper = styled.section`
-  width: ${props => (props.width ? props.width + 64 + 'px' : '734px')};
-  margin: 0px auto;
-  display: flex;
-  align-items: center;
+  @media (max-width: 890px) {
+  }
+  @media (min-width: 891px) {
+    width: ${props => (props.width ? props.width + 8 + 'px' : '734px')};
+    margin: 0px auto;
+  }
+  position: relative;
   svg {
-    color: #666;
+    cursor: pointer;
+  }
+  .prevBtn,
+  .nextBtn {
+    position: absolute;
+    top: 50%;
+    z-index: 1;
+    color: #66666663;
+  }
+  .prevBtn {
+    left: 10px;
+  }
+  .nextBtn {
+    right: 10px;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  @media (max-width: 890px) {
+    div {
+      transform: translate(
+        ${props =>
+          props.changeWidth
+            ? props.index * -props.changeWidth + 'px'
+            : props.index * -670 + 'px'},
+        0px
+      );
+      img {
+        width: ${props =>
+          props.changeWidth ? props.changeWidth + 'px' : '670px'};
+        height: auto;
+      }
+    }
+  }
+  @media (min-width: 891px) {
+    width: ${props => (props.width ? props.width + 'px' : '670px')};
+    border-radius: 8px;
+    div {
+      transform: translate(
+        ${props =>
+          props.width
+            ? props.index * -props.width + 'px'
+            : props.index * -670 + 'px'},
+        0px
+      );
+      img {
+        width: ${props => (props.width ? props.width + 'px' : '670px')};
+        height: auto;
+      }
+    }
+  }
+  height: ${props => (props.height ? props.height + 'px' : '500px')};
+  position: relative;
+  display: flex;
+  overflow: hidden;
+  div {
+    align-self: center;
+    transition: 0.6s;
     cursor: pointer;
   }
 `;
-const ImageWrapper = styled.div`
-  width: ${props => (props.width ? props.width + 'px' : '670px')};
-  height: ${props => (props.height ? props.height + 'px' : '500px')};
-  display: flex;
-  overflow: hidden;
-  border-radius: 20px;
-  div {
-    align-self: center;
-    transform: translate(
-      ${props =>
-        props.width
-          ? props.index * -props.width + 'px'
-          : props.index * -670 + 'px'},
-      0px
-    );
-    transition: 0.6s;
-    cursor: pointer;
-    img {
-      width: ${props => (props.width ? props.width + 'px' : '670px')};
-      height: auto;
-    }
+const DotWrapper = styled.span`
+  @media (max-width: 890px) {
+    width: ${props => props.changeWidth}px;
+  }
+  @media (min-width: 891px) {
+    width: 670px;
+  }
+  position: absolute;
+  height: 36px;
+  bottom: 0;
+  text-align: center;
+  box-shadow: inset 0px -20px 20px 0px #444444ba;
+`;
+
+const DotItem = styled.span`
+  svg {
+    margin: 0px 2px;
+    color: ${props => (props.isCurrent ? '#ffff' : '#8c8c8c63')};
   }
 `;
 export default ImageSlider;
