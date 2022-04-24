43f874e01dd5d876c5df60d2e80669528c09471b
SEOYEON HAN <tjdus0824@naver.com>
2022-04-25 02:21:14 +0900

    ADD: 마이페이지 간단한 레이아웃 작업

---
+++ src/pages/Mypage.js
@@ -0,0 +1,185 @@
+import React from 'react';
+import UserProfile from 'components/profile/UserProfile';
+import { FaRegSmileWink } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BsChatSquareDots } from 'react-icons/bs';
+import { IoIosArrowForward } from 'react-icons/io';
+import styled from 'styled-components';
+
+const user = {
+  id: '1',
+  nickname: '헛개수',
+  city: '부산',
+  district: '중구',
+};
+
+function Mypage() {
+  return (
+    <MainWrapper>
+      <InfoWrapper>
+        <UserInfo>
+          <UserProfile user={user} />
+        </UserInfo>
+        <Line />
+        <div className="temp">
+          78.2℃ <FaRegSmileWink className="smile" />
+        </div>
+        <div className="progress-bar">
+          <div className="progress">.</div>
+        </div>
+        <UserBox>
+          <div>
+            <AiOutlineHeart className="userBoxIcons" />
+            재거래 희망률 <span>100%</span>
+          </div>
+          <div className="userBoxSecond">
+            <BsChatSquareDots className="userBoxIcons" />
+            응답<span>99%</span>
+          </div>
+        </UserBox>
+      </InfoWrapper>
+      <UserLife>
+        {/* <SellProduct>판매상품</SellProduct>
+        <Neighborhood>동네생활</Neighborhood>
+        <Evaluation>받은 매너평가</Evaluation> */}
+        <div>
+          판매상품 <IoIosArrowForward className="rightArrow" />
+        </div>
+        <div>
+          동네생활 <IoIosArrowForward className="rightArrow" />
+        </div>
+        <div>
+          받은 매너평가 <IoIosArrowForward className="rightArrow" />
+        </div>
+      </UserLife>
+      <UserChat>
+        <div className="ChatBtn">
+          <span>판매자와 채팅하기</span>
+        </div>
+      </UserChat>
+    </MainWrapper>
+  );
+}
+
+const MainWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding-top: 70px;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const InfoWrapper = styled.section`
+  @media (max-width: 890px) {
+    padding: 0px 15px;
+  }
+  @media (min-width: 891px) {
+    width: 677px;
+    margin: 0px auto;
+  }
+  .temp {
+    /* float: right; */
+    display: flex;
+    align-items: center;
+    margin-left: 85%;
+    margin-bottom: 5px;
+    font-size: 18px;
+    color: #4f98ff;
+  }
+  .smile {
+    font-size: 25px;
+    margin-left: 5px;
+  }
+  .progress-bar {
+    width: 100%;
+    height: 19px;
+    background-color: #dedede;
+    font-weight: 600;
+    font-size: 12px;
+    border-radius: 25px;
+    margin-bottom: 30px;
+  }
+
+  .progress-bar .progress {
+    width: 78.2%;
+    height: 19px;
+    padding: 0;
+    /* text-align: center; */
+    background-color: #4f98ff;
+    color: #111;
+    border-radius: 25px;
+  }
+`;
+
+const Line = styled.div`
+  border-bottom: 1px solid #99999940;
+  margin-bottom: 30px;
+`;
+
+const UserBox = styled.div`
+  padding: 25px 35px;
+  border-radius: 3px;
+  background-color: whitesmoke;
+  div {
+    display: inline-block;
+  }
+  div > span {
+    margin-left: 3px;
+    font-weight: 600;
+  }
+  .userBoxSecond {
+    margin-left: 47%;
+  }
+  .userBoxIcons {
+    font-size: 20px;
+    margin: 0px 4px 0px 0px;
+  }
+`;
+const UserLife = styled.section`
+  @media (max-width: 890px) {
+    padding: 0px 15px;
+  }
+  @media (min-width: 891px) {
+    width: 677px;
+    margin: 0px auto;
+  }
+  div {
+    padding: 23px;
+    border-bottom: 1px solid #99999940;
+  }
+  .rightArrow {
+    font-size: 20px;
+    float: right;
+  }
+`;
+
+const UserChat = styled.div`
+  margin: 30px;
+  @media (max-width: 890px) {
+    padding: 0px 15px;
+  }
+  @media (min-width: 891px) {
+    width: 677px;
+    margin: 30px auto;
+  }
+  .ChatBtn {
+    border: 1px solid ${props => props.theme.signColor};
+    border-radius: 50px;
+    padding: 15px 20px;
+    cursor: pointer;
+    :hover {
+      background-color: ${props => props.theme.signColor};
+      span {
+        color: #ffff;
+      }
+    }
+    span {
+      color: ${props => props.theme.signColor};
+    }
+  }
+`;
+
+export default Mypage;
