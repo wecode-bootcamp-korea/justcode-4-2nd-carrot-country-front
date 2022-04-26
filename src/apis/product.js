f16feb06f1703df537f886b93a09a2c78c7442c2
Chae Won Suh <chaewon@chaewon-ui-Mac.local>
2022-04-26 09:50:43 +0900

    카테고리 fetch 완료, 로그인 구현 후에 post api 연결 필요

--- src/apis/product.js
+++ src/apis/product.js
@@ -6,4 +6,30 @@ async function getProductDetail(productId) {
     .then(data => data);
 }
 
-export { getProductDetail };
+function postProduct(allContents) {
+  console.log(allContents);
+  fetch(`${SERVER_PORT}/products`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      allContents,
+    }),
+  })
+    .then(res => res.json())
+    .then(result => {});
+}
+function postImage(props) {
+  console.log('postImage >>', props);
+  fetch(`${SERVER_PORT}/products/images`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'multiple/form-data' },
+    body: JSON.stringify({
+      imageURLs: props.imageURLs,
+    }),
+  })
+    .then(res => res.json())
+    .then(result => {});
+}
+export { getProductDetail, postProduct, postImage };
